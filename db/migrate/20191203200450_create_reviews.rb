87132b23d7356a3b11aa8c2bb6a5198d5088ecae
Rachel Lew <rlew07@gmail.com>
2019-12-03 13:17:02 -0700

    Create reviews table

---
+++ db/migrate/20191203200450_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.float :rating
+      t.string :content
+      t.string :image
+      t.timestamps
+    end
+  end
+end
