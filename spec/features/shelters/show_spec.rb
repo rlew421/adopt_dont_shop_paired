be88d9e5938a7e48ee4bb151b60f17fd0d45fb94
Rachel Lew <rlew07@gmail.com>
2019-12-03 12:58:30 -0700

    Add feature test to display list of reviews on shelter show page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe "shelter show page" do
   describe "as a visitor" do
     before(:each) do
       @shelter_1 = Shelter.create(name: "Gimme Shelter", address: "5218 Rolling Stones Avenue", city: "Denver", state: "CO", zip: 80203)
+      @review_1 = Review.create!(title: "Would Recommend", rating: 4, content: "Good service, easy application process!")
+      @review_2 = Review.create!(title: "Alright", rating: 3, content: "Nothing special, but they had an ok selection of pets and were quite clean.")
+      @review_3 = Review.create!(title: "Awesome!", rating: 4, content: "They helped us adopt the perfect dog!", image: "http://www.westdeertownship.com/wp-content/uploads/2015/11/KIMG1537.jpg")
 
       visit "/shelters/#{@shelter_1.id}"
     end
@@ -26,6 +29,22 @@ RSpec.describe "shelter show page" do
     it "I see a link that takes me to that shelter's pets page" do
       click_link "View #{@shelter_1.name}'s Pets"
       expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets")
+    end
+
+    it "I see a list of reviews for that shelter where each review has a:
+      - title
+      - rating
+      - content
+      - optional picture" do
+
+      expect(page).to have_content(@review_1.title)
+      expect(page).to have_content(@review_1.rating)
+      expect(page).to have_content(@review_2.content)
+      expect(page).to have_content(@review_2.title)
+      expect(page).to have_content(@review_2.rating)
+      expect(page).to have_content(@review_2.content)
+      expect(page).to have_content(@review_3.title)
+      expect(page).to have_css("img[src*='#{@review_3.image}']")
     end
   end
 end
