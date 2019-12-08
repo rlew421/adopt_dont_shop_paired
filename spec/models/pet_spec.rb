15deeaf9ebc8bd38baf8152ae8cfb755723935fa
Paul Debevec <paulmdebevec@gmail.com>
2019-12-08 14:58:40 -0700

    Updated pet_spec test to include the many-to-many
    Co-Authored-By: Rachel Lew <rlew07@gmail.com>

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -12,6 +12,8 @@ describe Pet, type: :model do
 
   describe "relationships" do
     it {should belong_to :shelter}
+    it {should have_many :application_pets}
+    it {should have_many(:applications).through(:application_pets)}
   end
 
   describe "class methods" do
