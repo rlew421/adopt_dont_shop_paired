40d7bded603253dac1c8cd9f2cfaec85bacab42f
Paul Debevec <paulmdebevec@gmail.com>
2019-12-09 15:19:54 -0700

    Added test to verify all fields are filled in on the application
    Co-Authored-By: Rachel Lew <48839191+rlew421@users.noreply.github.com>

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -61,4 +61,47 @@ RSpec.describe "when I visit my favorites page" do
     expect(page).to have_content(pet_3.name)
     expect(page).to have_css("img[src*='#{pet_3.image}']")
   end
+
+  it "When I fail to fill out any field I am redirected back to application form and receive flash message" do
+
+    shelter_1 = Shelter.create!(name: "Denver Dog Shelter", address: "7893 Colfax", city: "Denver", state: "CO", zip: 80209)
+    pet_1 = shelter_1.pets.create!(image: "https://i.pinimg.com/564x/59/71/31/5971314eb28926a1ccc298396f099189.jpg",
+                        name: 'Pet 1',
+                        description: "Pet 1 description",
+                        approximate_age: 7,
+                        sex: 'Male')
+
+    pet_2 = shelter_1.pets.create!(image: "https://thumbs-prod.si-cdn.com/wLrciMDDerMdsUZS8GZwduuMvPs=/420x240/filters:focal(274x157:275x158)/https://public-media.si-cdn.com/filer/ca/15/ca15e676-bfa1-4180-ae4b-02797b55c093/gettyimages-511711532_720.jpg",
+                        name: 'Pet 2',
+                        description: "Pet 2 description",
+                        approximate_age: 2,
+                        sex: 'Female')
+    pet_3 = shelter_1.pets.create!(image: "https://vetstreet.brightspotcdn.com/dims4/default/3407f3b/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Ffb%2F31%2F032a6aae436a9821acda211044fb%2Fbulldog-ap-rn4myi-645.jpg",
+                        name: 'Pet 3',
+                        description: "Pet 2 description",
+                        approximate_age: 4,
+                        sex: 'Male')
+
+    visit "/pets/#{pet_1.id}"
+    click_button "Add #{pet_1.name} to Favorites"
+
+    visit "/pets/#{pet_2.id}"
+    click_button "Add #{pet_2.name} to Favorites"
+
+    visit "/pets/#{pet_3.id}"
+    click_button "Add #{pet_3.name} to Favorites"
+
+    visit '/favorites'
+    click_link "Apply to Adopt Pets"
+
+    fill_in :name, with: "John Doe"
+    fill_in :address, with: "1703 11th Ave"
+    fill_in :city, with: "Boulder"
+    fill_in :phone_number, with: 7209305236
+    fill_in :description, with: "I'm just searching for a new best friend"
+    click_button "Submit Application"
+
+    expect(current_path).to eq('/applications/new')
+    expect(page).to have_content("State can't be blank and Zip can't be blank")
+  end
 end
