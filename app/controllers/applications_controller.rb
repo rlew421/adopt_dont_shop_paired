be72657b317194838d623bc96baf611f64e7a2ab
Rachel Lew <rlew07@gmail.com>
2019-12-09 14:33:18 -0700

    Refactor create method
    Co-Authored-By: Paul Debevec <paulmdebevec@gmail.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,12 +8,11 @@ class ApplicationsController < ApplicationController
   end
 
   def create
-    pet_favorites = favorites.contents
-    @favorited_pets = pet_favorites.map do |pet_id|
-      Pet.find(pet_id)
-    end
-    @favorited_pets.each do |pet|
-      pet.applications.create(application_params)
+    checked_pets = Pet.where(id: params[:favorites])
+    application = Application.create(application_params)
+    checked_pets.each do |pet|
+      pet.applications << application
+      favorites.remove_pet(pet.id)
     end
     redirect_to '/favorites'
     flash[:success] = 'Your application has been submitted for the selected pets!'
