02e37c57615b5a950caaf739a2fb11713b08cd29
Paul Debevec <paulmdebevec@gmail.com>
2019-12-10 21:53:14 -0700

    Added test to verify reviews are being deleted with shelter.Testing 100%

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "when I visit a shelter show page" do
     describe "when I click on a link to delete the shelter" do
       before :each do
         @shelter_1 = Shelter.create(name: "Gimme Shelter", address: "5218 Rolling Stones Avenue", city: "Denver", state: "CO", zip: 80203)
+        @review_1 = @shelter_1.reviews.create!(title: "Perfectly adequate", rating: 3, content: "Run of the mill shelter")
       end
       it "I can delete a shelter without pets" do
 
@@ -20,6 +21,8 @@ RSpec.describe "when I visit a shelter show page" do
         pet_2 = @shelter_1.pets.create!(name: "Pet 2", image: "https://www.certapet.com/wp-content/uploads/2019/05/shutterstock_561362863.jpg", description: "It's pet #2.", approximate_age: 2, sex: "Male")
         pet_3 = @shelter_1.pets.create!(name: "Pet 3", image: "https://www.certapet.com/wp-content/uploads/2019/05/shutterstock_561362863.jpg", description: "It's pet #3.", approximate_age: 1, sex: "Female")
 
+        expect(Review.count).to eql(1)
+        
         visit "/shelters/#{@shelter_1.id}"
         click_link "Delete #{@shelter_1.name}"
 
@@ -30,6 +33,7 @@ RSpec.describe "when I visit a shelter show page" do
         expect(page).to_not have_content(pet_1.name)
         expect(page).to_not have_content(pet_2.name)
         expect(page).to_not have_content(pet_3.name)
+        expect(Review.count).to eql(0)
       end
     end
   end
