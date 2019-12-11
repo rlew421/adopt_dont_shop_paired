c70c1954fcbaa0bfd543a009d06b0e14dc7cf37d
Paul Debevec <paulmdebevec@gmail.com>
2019-12-11 11:52:29 -0700

    Added flash message to create method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -19,8 +19,12 @@ class PetsController < ApplicationController
   def create
     shelter = Shelter.find(params[:shelter_id])
     pet = Pet.new(pet_params.merge(shelter_id: shelter.id))
-    pet.save
-    redirect_to "/shelters/#{shelter.id}/pets"
+    if pet.save
+      redirect_to "/shelters/#{shelter.id}/pets"
+    else
+      flash[:error] = pet.errors.full_messages.to_sentence
+      redirect_to "/shelters/#{shelter.id}/pets/new"
+    end
   end
 
   def edit
